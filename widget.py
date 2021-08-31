ddeade46003f5f075492523b81144021a1336bb0
Kaelem <kaelem.chandra@outlook.com>
2021-09-01 08:51:02 +1200

    add window boilerplate code

---
+++ widget.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+class App(QMainWindow):
+
+        # class constructor
+        def __init__(self) -> None:
+            super().__init__()
+            # window settings
+            self.width = 600
+            self.height = 250
+            self.setWindowTitle("WidgetApp")
+            self.setFixedSize(self.width, self.height)
+
+# main process
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    # create an object of class myApp
+    appWindow = App()
+    # show the window
+    App.show(appWindow)
+    sys.exit(app.exec_()) 
